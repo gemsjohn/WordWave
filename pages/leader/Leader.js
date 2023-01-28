5b18c96890409fd3db5c6da47010e0bdef53ecda
JRM <johnrhmartin@gmail.com>
2023-01-28 14:52:54 -0500

    pushing for good measure

--- pages/leader/Leader.js
+++ pages/leader/Leader.js
@@ -10,21 +10,21 @@ import { Styling } from '../../Styling';
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
 
-export const LeaderScreen = ({ navigation }) => {  
-  
-    const { data: leaderboard, refetch } = useQuery(GAMES);
-    // console.log(leaderboard)
-  
-    const DATA = leaderboard?.games;
-    // console.log(DATA)
-  
-  
-    const Item = ({ username, score, pos }) => (
-      <>
+export const LeaderScreen = ({ navigation }) => {
+
+  const { data: leaderboard, refetch } = useQuery(GAMES);
+  // console.log(leaderboard)
+
+  const DATA = leaderboard?.games;
+  // console.log(DATA)
+
+
+  const Item = ({ username, score, pos }) => (
+    <>
       <View key={pos}>
         <View
           style={{
-            backgroundColor: 'rgba(0, 0, 0, 0.5)',
+            // backgroundColor: 'rgba(0, 0, 0, 0.5)',
             height: 60,
             width: windowWidth - 80,
             alignSelf: 'center',
@@ -33,7 +33,7 @@ export const LeaderScreen = ({ navigation }) => {
           }}
         >
           <View style={{ flexDirection: 'column', alignSelf: 'center' }}>
-              <View style={{ flexDirection: 'row', alignSelf: 'center' }}>
+            <View style={{ flexDirection: 'row', alignSelf: 'center' }}>
               <Text
                 style={{
                   color: 'white',
@@ -46,82 +46,89 @@ export const LeaderScreen = ({ navigation }) => {
               >
                 {pos}
               </Text>
-                <Text
-                  style={{
-                    fontSize: 30,
-                    fontWeight: 'bold',
-                    color: '#53f4a4',
-                    width: 180
-                  }}
-                  numberOfLines={1}
-                  ellipsizeMode='tail'
-                  allowFontScaling={false}
-                >
-                  {username}
-                </Text>
-                <Text
-                  style={{
-                    fontSize: 25,
-                    fontWeight: 'bold',
-                    color: 'white',
-                    alignSelf: 'flex-end',
-                    textAlign: 'right',
-                    width: 180
-                  }}
-                  numberOfLines={1}
-                  ellipsizeMode='tail'
-                  allowFontScaling={false}
-                >
-                  {score}
-                </Text>
-              </View>
+              <Text
+                style={{
+                  fontSize: 30,
+                  fontWeight: 'bold',
+                  color: '#53f4a4',
+                  width: 180
+                }}
+                numberOfLines={1}
+                ellipsizeMode='tail'
+                allowFontScaling={false}
+              >
+                {username}
+              </Text>
+              <Text
+                style={{
+                  fontSize: 25,
+                  fontWeight: 'bold',
+                  color: 'white',
+                  alignSelf: 'flex-end',
+                  textAlign: 'right',
+                  width: 180
+                }}
+                numberOfLines={1}
+                ellipsizeMode='tail'
+                allowFontScaling={false}
+              >
+                {score}
+              </Text>
+            </View>
 
           </View>
         </View>
         {/* <View style={Styling.profileDivisionLine}></View> */}
       </View>
-      <View style={{marginBottom: 10}}></View>
-      </>
-    );
-  
-    const renderItem = ({ item }) => (
-      <Item username={item.username} score={item.score} pos={item.position} />
-    );
-  
-    useEffect(() => {
-      refetch();
-    }, [])
-  
-    return (
-      <>
-      <View style={{backgroundColor: 'black', height: '100%'}}>
-        
-        
-        <View
+      <View style={{ marginBottom: 10 }}></View>
+    </>
+  );
+
+  const renderItem = ({ item }) => (
+    <Item username={item.username} score={item.score} pos={item.position} />
+  );
+
+  useEffect(() => {
+    refetch();
+  }, [])
+
+  return (
+    <>
+      <View style={{ backgroundColor: 'black', height: '100%' }}>
+        <ImageBackground
+          source={require('../../assets/background_leaderboard.png')}
+          resizeMode="cover"
           style={{
-            alignSelf: 'center',
-            // marginTop: WidthRatio(30)
+            justifyContent: 'center',
+            height: '100%'
+          }}>
 
-          }}
-        >
-          <View style={{alignSelf: 'center', flexDirection: 'column', padding: 10, borderRadius: 50, width: 350, marginTop: 80}}>
-            <Text style={{color: 'yellow', fontSize: 40, fontWeight: 'bold', alignSelf: 'center'}}>HIGH SCORES</Text>
-            {/* <Text style={{color: 'white', fontSize: HeightRatio(20), alignSelf: 'center'}}>Last 30 Days</Text> */}
+          <View
+            style={{
+              alignSelf: 'center',
+              // marginTop: WidthRatio(30)
+
+            }}
+          >
+            <View style={{ alignSelf: 'center', flexDirection: 'column', padding: 10, borderRadius: 50, width: 350, marginTop: 80 }}>
+              <Text style={{ color: 'yellow', fontSize: 40, fontWeight: 'bold', alignSelf: 'center' }}>HIGH SCORES</Text>
+              {/* <Text style={{color: 'white', fontSize: HeightRatio(20), alignSelf: 'center'}}>Last 30 Days</Text> */}
+            </View>
+            <SafeAreaView style={Styling.flatlistContainer}>
+              <FlatList
+                data={DATA}
+                renderItem={renderItem}
+                keyExtractor={item => item.id}
+              />
+              <View style={{ marginBottom: 70 }}></View>
+              <Navbar nav={navigation} position={'relative'} from={'leader'} />
+
+            </SafeAreaView>
           </View>
-          <SafeAreaView style={Styling.flatlistContainer}>
-            <FlatList
-              data={DATA}
-              renderItem={renderItem}
-              keyExtractor={item => item.id}
-            />
-            <View style={{marginBottom: 70}}></View>
-          <Navbar nav={navigation} position={'relative'} from={'leader'} />
+        </ImageBackground>
 
-          </SafeAreaView>
-        </View>
-        
       </View>
-      
+
 
       <StatusBar
         barStyle="default"
@@ -131,5 +138,5 @@ export const LeaderScreen = ({ navigation }) => {
         networkActivityIndicatorVisible={true}
       />
     </>
-    );
-  }
+  );
+}
